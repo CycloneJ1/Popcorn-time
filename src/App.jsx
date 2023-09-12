845929e39d904f5a196625cc72837efb84da3b31
Cyclonej <cyclonejmoto@gmail.com>
2023-09-12 16:58:43 +0200

    Popcorn time working

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import './App.css'
 import Header from './components/Header'
-import Main from './components/Main'
 import Footer from './components/Footer'
+import Main from './components/Main'
 
+
+import './App.css'
 
 function App() {
   return (
-
-    <Footer />,
-    <Header />,
-    <Main />
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   )
 }
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,68 @@
-function Main(){
-    return <h1>this is the Main</h1>;
+import { useState } from "react";
+import movies from "../data/movies.json";
+
+import "./Main.css";
+
+function Main() {
+
+    // const [variable, setVariable] = useState(initalValue);
+
+    const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
+
+    const deleteMovie = (id) => {
+        console.log("deleting movie with id", id);
+        const newList = moviesToDisplay.filter((movie) => {
+            return movie.id !== id;
+        });
+
+        // const newList = moviesToDisplay.filter( (element) => {
+        //     if(element.id !== Id){
+        //         return true;
+        //     } else {
+        //         return false;
+        //     }
+        // });
+
+        setMoviesToDisplay(newList);
+    }
+
+
+    let message;
+    if (moviesToDisplay.length > 0) {
+        message = <h1>Number of movies: {moviesToDisplay.length}</h1>
+    } else {
+        message = <h1>Sorry, no movies to display</h1>
+    }
+
+
+
+    return (
+        <div className="Main">
+            
+            {message}
+
+            {moviesToDisplay.map((movieObj) => {
+                return (
+                    <section key={movieObj.id} className="card" >
+                        <h2>{movieObj.title}</h2>
+
+                        { movieObj.imgURL 
+                            ? <img src={movieObj.imgURL} /> 
+                            : <img src="https://dummyimage.com/182x268/ffffff/000000" /> 
+                        }
+
+                        
+
+
+                        <h3>Rating: {movieObj.rating}</h3>
+                        {movieObj.rating > 8 && <p>RECOMMENDED</p>}
+
+                        <button onClick={() => {deleteMovie(movieObj.id)}}>Delete</button>
+                    </section>
+                )
+            })}
+        </div>
+    );
 }
 
 export default Main;
