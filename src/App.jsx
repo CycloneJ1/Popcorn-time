915e5d6f5bd5b59b0bc5de4fc2cef7439752bab0
Cyclonej <cyclonejmoto@gmail.com>
2023-09-13 15:12:51 +0200

    formed functionality for adding new movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,98 @@
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Main from './components/Main'
+import { useState } from "react";
 
+import movies from "./data/movies.json";
+
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
 
 import './App.css'
 
 function App() {
+
+  const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
+
+  const [title, setTitle] = useState("");
+  const [rating, setRating] = useState("");
+  const [image, setImage] = useState("")
+  
+
+  const deleteMovie = (movieTitle) => {
+    const newList = moviesToDisplay.filter( (element) => {
+        return element.title !== movieTitle;
+    });
+    setMoviesToDisplay(newList);
+  }
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const newMovie = {
+      title: title,
+      image: image,
+      rating:rating
+    }
+
+    const newList = [newMovie, ...moviesToDisplay];
+    
+    setMoviesToDisplay(newList);
+
+    // clear form
+    setTitle("");
+    setRating("")
+  }
+
+
   return (
     <>
-      <Header />
-      <Main />
+      <Header numberOfMovies={moviesToDisplay.length} />
+
+      <section>
+        <form onSubmit={handleSubmit}>
+          
+          <label>
+            Title: 
+            <input 
+              type="text" 
+              name="title" 
+              required={true}
+              placeholder="enter the title" 
+              value={title} 
+              onChange={(e) => { setTitle(e.target.value) }}
+            />
+          </label>
+          
+{/*                     
+          <label>
+            ImgURL:
+            <input 
+            name="imgURL"
+            value={imgURL}
+            onChange={(e) => { setRating(e.target.value) }}
+
+            />
+          </label> */}
+          
+          <label>
+            Rating:
+            <input 
+              name="rating"
+              type="number"
+              required={true}
+              min={1}
+              max={10}
+              value={rating}
+              onChange={(e) => { setRating(e.target.value) }}
+            />
+          </label>
+
+
+          <button>Create</button>
+        </form>
+      </section>
+
+      <Main movies={moviesToDisplay} callbackToDelete={deleteMovie} />
       <Footer />
     </>
   )
